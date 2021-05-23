8beed331c357c2701040e4f59f36396baaf1895d
rruyzz <rodolforuyz@gmail.com>
2021-05-23 14:53:10 -0300

    tela de login

--- app/src/main/java/dominando/android/moviesdb/splash/SplashActivity.kt
+++ app/src/main/java/dominando/android/moviesdb/splash/SplashActivity.kt
@@ -1,12 +1,24 @@
 package dominando.android.moviesdb.splash
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
 import dominando.android.moviesdb.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setNav()
+    }
+
+    private fun setNav(){
+        val myNavHostFragment : NavHostFragment = fragment as NavHostFragment
+        val inflater = myNavHostFragment.navController.navInflater
+        myNavHostFragment.navController.graph = inflater.inflate(R.navigation.login_graph)
     }
 }
