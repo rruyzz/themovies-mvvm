d5dd86c1e8582dc34c6cdaf329e31269a03e1d35
Rodolfo <rodolfo@xds.com.br>
2021-11-11 10:45:53 -0300

    barra na activity

--- app/src/main/java/dominando/android/moviesdb/MainActivity.kt
+++ app/src/main/java/dominando/android/moviesdb/MainActivity.kt
@@ -2,10 +2,33 @@ package dominando.android.moviesdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import dominando.android.moviesdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bundle = savedInstanceState ?: intent.extras
+        setupNavigationController(bundle)
+        setButtons()
+    }
+
+    private fun setupNavigationController(bundle: Bundle?) {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.home_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.setGraph(R.navigation.main_graph)
+    }
+
+    private fun setButtons() = with(binding){
+        icProfile.setOnClickListener { navController.setGraph(R.navigation.profile_graph) }
+        icSearch.setOnClickListener { navController.setGraph(R.navigation.main_graph) }
     }
 }
---
+++ app/src/main/java/dominando/android/moviesdb/profile/ProfileFragment.kt
@@ -0,0 +1,21 @@
+package dominando.android.moviesdb.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dominando.android.moviesdb.R
+
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
